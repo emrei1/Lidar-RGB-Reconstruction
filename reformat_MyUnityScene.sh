f9ab2aa260a6365d941c0c3a37bcf3aa606ae077
Emre Inceoglu <47436973+emrei1@users.noreply.github.com>
2026-01-15 13:31:35 +0300

    modified reformat_MyUnityScene.sh

--- reformat_MyUnityScene.sh
+++ reformat_MyUnityScene.sh
@@ -4,7 +4,12 @@ set -e  # herhangi bir hata olursa script dursun
 
 echo "🔧 MyUnityScene folder is being re-formatted..."
 
+apt update
+apt install unzip
+
 wget https://github.com/emrei1/Lidar-RGB-Reconstruction/releases/download/v1.0-data/MyUnityScene.zip
+
+unzip MyUnityScene.zip
 
 python reformat_myunityscene.py
 
